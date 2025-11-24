db669a0c1f58116c61e7feaf768408a2573da6a4
Seger01 <segersars@gmail.com>
2025-11-24 22:20:14 +0100

    got a nice and working matugen config working

--- scripts/.config/scripts/wallSelect.sh
+++ scripts/.config/scripts/wallSelect.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-WALLPAPER_DIR="$HOME/.config/wallpapers/Wallpapers_orig/"
+WALLPAPER_DIR="$HOME/.config/wallpapers/wallpapers/"
 
 menu() {
     find "${WALLPAPER_DIR}" -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.gif" \) | awk '{print "img:"$0}'
@@ -11,7 +11,9 @@ main() {
     choice=$(menu | wofi -c ~/.config/wofi/style-wallpaper-conf -s ~/.config/wofi/style-wallpaper.css --show dmenu --prompt "Select Wallpaper:" -n)
     selected_wallpaper=$(echo "$choice" | sed 's/^img://')
     swww img "$selected_wallpaper" --transition-type any --transition-fps 30 --transition-duration 4
-    matugen image $selected_wallpaper -t scheme-tonal-spot  --contrast 0.07
+    matugen image $selected_wallpaper -t scheme-fruit-salad --show-colors >> ~/.config/scripts/matugen_colors.txt
+    # matugen image $selected_wallpaper -t scheme-tonal-spot 
+    # matugen image $selected_wallpaper -t scheme-fruit-salad --show-colors >> ~/.config/scripts/matugen_colors.txt
 }
 main
 
