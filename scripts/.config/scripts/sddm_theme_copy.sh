f834a99906dba4a13100e9659167a76c381b3f3e
Seger01 <segersars@gmail.com>
2026-02-19 22:21:31 +0100

    added papirus icons to repo

--- scripts/.config/scripts/sddm_theme_copy.sh
+++ scripts/.config/scripts/sddm_theme_copy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-WALLPAPER="/home/seger/.dotfiles/scripts/.config/scripts/theme-switcher/backgrounds/spaceduck/wallhaven-e78y9r_2560x1440.png"
+WALLPAPER="/home/seger/.dotfiles/scripts/.config/scripts/theme-switcher/backgrounds/spaceduck/wallhaven-e719q8_2560x1440.png"
 
 cp /home/seger/.config/theme-switcher-sddm/sddm.conf /usr/share/sddm/themes/silent/configs/custom.conf
 
